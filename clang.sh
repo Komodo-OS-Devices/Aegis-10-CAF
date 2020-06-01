8522f548748d5f59ab4fb24cc0ab5a7d5a5e4d67
Wahyu Hidayat <xxwahyuxx47@gmail.com>
2020-06-01 10:35:32 +0700

    Add Aegis File
    Signed-off-by: Wahyu Hidayat <xxwahyuxx47@gmail.com>

---
+++ clang.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+export KBUILD_BUILD_USER="Wahyu"
+export KBUILD_BUILD_HOST="Aegis-Build"
+export ARCH=arm64
+
+make O=out ARCH=arm64 aegis_defconfig
+
+PATH="/home/wahyu/project/dtc/bin:/home/wahyu/project/gcc/bin:$PATH" \
+
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+			2>&1 | tee log.jancok
