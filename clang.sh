e8a9c3e695bdbfb70cfcef2d392646ecfe01f4b6
Wahyu Hidayat <xxwahyuxx47@gmail.com>
2020-06-09 07:25:19 +0700

    clang.sh: change compiler

--- clang.sh
+++ clang.sh
@@ -2,10 +2,11 @@
 export KBUILD_BUILD_USER="Wahyu"
 export KBUILD_BUILD_HOST="Aegis-Build"
 export ARCH=arm64
+export CROSS_COMPILE_ARM32=/home/wahyu/project/gcc49arm/bin/arm-linux-androideabi-
 
 make O=out ARCH=arm64 aegis_defconfig
 
-PATH="/home/wahyu/project/dtc/bin:/home/wahyu/project/gcc/bin:$PATH" \
+PATH="/home/wahyu/project/clang10/bin:/home/wahyu/project/gcc/bin:$PATH" \
 
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
