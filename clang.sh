ed1acb76feaf4097e729db3ae78e7701a33db467
Wahyu Hidayat <xxwahyuxx47@gmail.com>
2020-07-27 11:01:42 +0700

    add build script

---
+++ clang.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+export KBUILD_BUILD_USER="Wahyu"
+export KBUILD_BUILD_HOST="Aegis-Build"
+export ARCH=arm64
+export CROSS_COMPILE_ARM32=/home/wahyu/project/gcc49arm/bin/arm-linux-androideabi-
+
+make O=out ARCH=arm64 aegis_defconfig
+
+PATH="/home/wahyu/project/dtc/bin:/home/wahyu/project/gcc/bin:$PATH" \
+
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+			2>&1 | tee log.jancok
+
